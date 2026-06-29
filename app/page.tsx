8a65defeb00cc8bf0119c688a29e4752fdd4a3ac
Swaraj <swarajunde1@gmail.com>
2026-06-29 19:39:31 +0530

    layout and main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
+import UserButton from "@/modules/auth/components/user-button";
 import Image from "next/image";
 
 export default async function Home() {
 
   return (
     <div>
-      <Button></Button>
+      <Button>Get Started</Button>
+      <UserButton/>
     </div>
   );
 }
