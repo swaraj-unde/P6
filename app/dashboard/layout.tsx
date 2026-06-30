2f583dc6b9543136c4cbbb5b46914ed0dd3da2e5
Swaraj <swarajunde1@gmail.com>
2026-06-30 18:02:23 +0530

    dashboard layout

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,37 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { getAllPlaygroundForUser } from "@/modules/dashboard/actions";
+import { DashboardSidebar } from "@/modules/dashboard/components/dashboard-sidebar";
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const playgroundData = await getAllPlaygroundForUser();
+  const technologyIconMap: Record<string, string> = {
+    REACT: "Zap",
+    NEXTJS: "Lightbulb",
+    EXPRESS: "Database",
+    VUE: "Compass",
+    HONO: "FlameIcon",
+    ANGULAR: "Terminal",
+  };
+
+  const formattedPlaygroundData = playgroundData?.map((item) => ({
+    id: item.id,
+    name: item.title,
+    icon: technologyIconMap[item.template] || "Code",
+    // @ts-ignore
+    starred: item.Starmark?.[0]?.isMarked || false,
+  }));
+
+  return (
+    <SidebarProvider>
+      <div className="flex min-h-screen w-full overflow-x-hidden">
+        {/* @ts-ignore */}
+        <DashboardSidebar initialPlaygroundData={formattedPlaygroundData} />
+        <main className="flex-1">{children}</main>
+      </div>
+    </SidebarProvider>
+  );
+}
